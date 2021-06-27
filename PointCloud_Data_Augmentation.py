f1ecb7aa83dc77b3716dcf950f4017745b47b5e1
MTawfik <m.tawfik@live.com>
2021-06-27 17:06:16 +0200

    Editted Code to origin

--- PointCloud_Data_Augmentation.py
+++ PointCloud_Data_Augmentation.py
@@ -105,7 +105,7 @@ def Jitter_noise(sigma, clip):
     
     return new_pcd
 
-# Translate Scaled Function to the Ground
+# Translate Scaled Function to the Ground; In case the object is needed on the ground plane after scaling.
 
 def TransToGnd(ScaleFunction):
 
@@ -121,8 +121,8 @@ def TransToGnd(ScaleFunction):
 
 # Import PCD file
 
-def import_pt(m):
-    pcd = o3d.io.read_point_cloud(m)
+def import_pt(pt):
+    pcd = o3d.io.read_point_cloud(pt)
     return pcd
 
 #############################
@@ -131,21 +131,21 @@ def import_pt(m):
 
 i = 1
 
-Dataset = r'F:\Work\Comark\Trucks augmented data\Transit' #Path to Dataset
+Dataset = r'Path to Dataset' 
 
-RandomDownsample_Folder = r'F:\Work\Comark\Trucks augmented data\Transit\RandomDownsample' #Path to RDownsample Folder
-UniformDownsample_Folder = r'F:\Work\Comark\Trucks augmented data\Transit\UniformDownsample' #Path to UDownsample Folder
-Scale_Folder = r'F:\Work\Comark\Trucks augmented data\Transit\Scale' #Path to Scale Folder
-Rotate_Folder = r'F:\Work\Comark\Trucks augmented data\Transit\Rotate' #Path to Rotate Folder
-Translate_Folder = r'F:\Work\Comark\Trucks augmented data\Transit\Translate' #Path to Translate Folder
-Jitter_Folder = r'F:\Work\Comark\Trucks augmented data\Transit\Jitter'
+RandomDownsample_Folder = r'Path to Random Downsampling folder' 
+UniformDownsample_Folder = r'Path to Uniform Downsampling folder' 
+Scale_Folder = r'Path to Scale folder' 
+Rotate_Folder = r'Path to Rotate folder' 
+Translate_Folder = r'Path to Translate folder' 
+Jitter_Folder = r'Path to Jitter folder'
 
 if os.path.isdir(RandomDownsample_Folder) and os.path.isdir(UniformDownsample_Folder) and \
     os.path.isdir(Scale_Folder) and os.path.isdir(Rotate_Folder) and os.path.isdir(Translate_Folder)\
         and os.path.isdir(Jitter_Folder) == True: # Checks if the folders are there
     pass
 else:
-    os.mkdir(RandomDownsample_Folder) # If the folders aren't there, Create them and do the same process
+    os.mkdir(RandomDownsample_Folder) # If the folders aren't there, Create them
     os.mkdir(UniformDownsample_Folder)
     os.mkdir(Rotate_Folder)
     os.mkdir(Scale_Folder)
@@ -161,51 +161,51 @@ for filename in os.listdir(Dataset): # Scans the directory for files
         # Apply the functions on each file
         RDown = RDownsample_Point(Sampling_Ratio)
         UDown = UDownsample_Point(Sampling_Rate)
-        #RotateX, RotateY, RotateZ = rotate_point(rotation_angle)
-        #scaling = scale_point(scale)
-        #scaling = TransToGnd(scale_point(scale))
-        #translateX, translateY, translateZ  = translate_point(translation)
+        RotateX, RotateY, RotateZ = rotate_point(rotation_angle)
+        scaling = scale_point(scale)
+        #scaling = TransToGnd(scale_point(scale)) 
+        translateX, translateY, translateZ  = translate_point(translation)
         Jitter = Jitter_noise(sigma, clipper)
 
-        New_filename_RD = str("RandomDown_Transit_") + str(i) + str(".pcd") # Renames the new files
-        New_filename_UD = str("UniformDown_Transit_") + str(i) + str(".pcd")
-        #New_filename_S = str("Scale_Scooter_") + str(i) + str(".pcd")
-        #New_filename_J = str("Jitter_Transit_") + str(i) + str(".pcd")
-        New_filename_J = str("Jitter_Transit_") + str(i) + str(".pcd")
+        New_filename_RD = str("RandomDown_") + str(i) + str(".pcd") # Renames the new files
+        New_filename_UD = str("UniformDown_") + str(i) + str(".pcd")
+        New_filename_S = str("Scale_") + str(i) + str(".pcd")
+        New_filename_J = str("Jitter_") + str(i) + str(".pcd")
+        New_filename_J = str("Jitter_") + str(i) + str(".pcd")
 
-        # New_filename_RX = str("Rotate_ScooterX_") + str(i) + str(".pcd")
-        # New_filename_RY = str("Rotate_ScooterY_") + str(i) + str(".pcd")
-        # New_filename_RZ = str("Rotate_ScooterZ_") + str(i) + str(".pcd")
+        New_filename_RX = str("Rotate_X_") + str(i) + str(".pcd")
+        New_filename_RY = str("Rotate_Y_") + str(i) + str(".pcd")
+        New_filename_RZ = str("Rotate_Z_") + str(i) + str(".pcd")
 
-        # New_filename_TX = str("Translate_ScooterX_") + str(i) + str(".pcd")
-        # New_filename_TY = str("Translate_ScooterY_") + str(i) + str(".pcd")
-        # New_filename_TZ = str("Translate_ScooterZ_") + str(i) + str(".pcd")
+        New_filename_TX = str("Translate_X_") + str(i) + str(".pcd")
+        New_filename_TY = str("Translate_Y_") + str(i) + str(".pcd")
+        New_filename_TZ = str("Translate_Z_") + str(i) + str(".pcd")
 
         file_path_RD = os.path.join(RandomDownsample_Folder, New_filename_RD) # Forms the new path for the files
         file_path_UD = os.path.join(UniformDownsample_Folder, New_filename_UD)
-        #file_path_S = os.path.join(Scale_Folder, New_filename_S)
+        file_path_S = os.path.join(Scale_Folder, New_filename_S)
         file_path_J = os.path.join(Jitter_Folder, New_filename_J)
 
-        # file_path_RX = os.path.join(Rotate_Folder, New_filename_RX)
-        # file_path_RY = os.path.join(Rotate_Folder, New_filename_RY)
-        # file_path_RZ = os.path.join(Rotate_Folder, New_filename_RZ)
+        file_path_RX = os.path.join(Rotate_Folder, New_filename_RX)
+        file_path_RY = os.path.join(Rotate_Folder, New_filename_RY)
+        file_path_RZ = os.path.join(Rotate_Folder, New_filename_RZ)
 
-        # file_path_TX = os.path.join(Translate_Folder, New_filename_TX)
-        # file_path_TY = os.path.join(Translate_Folder, New_filename_TY)
-        # file_path_TZ = os.path.join(Translate_Folder, New_filename_TZ)
+        file_path_TX = os.path.join(Translate_Folder, New_filename_TX)
+        file_path_TY = os.path.join(Translate_Folder, New_filename_TY)
+        file_path_TZ = os.path.join(Translate_Folder, New_filename_TZ)
 
         o3d.io.write_point_cloud(file_path_RD, RDown, write_ascii=True) # Writes the new pcd files to the folders
         o3d.io.write_point_cloud(file_path_UD, UDown, write_ascii=True)
-        #o3d.io.write_point_cloud(file_path_S, scaling, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_S, scaling, write_ascii=True)
         o3d.io.write_point_cloud(file_path_J, Jitter, write_ascii=True)
 
-        #o3d.io.write_point_cloud(file_path_RX, RotateX, write_ascii=True)
-        #o3d.io.write_point_cloud(file_path_RY, RotateY, write_ascii=True)
-        #o3d.io.write_point_cloud(file_path_RZ, RotateZ, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_RX, RotateX, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_RY, RotateY, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_RZ, RotateZ, write_ascii=True)
         
-        #o3d.io.write_point_cloud(file_path_TX, translateX, write_ascii=True)
-        #o3d.io.write_point_cloud(file_path_TY, translateY, write_ascii=True)
-        #o3d.io.write_point_cloud(file_path_TZ, translateZ, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_TX, translateX, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_TY, translateY, write_ascii=True)
+        o3d.io.write_point_cloud(file_path_TZ, translateZ, write_ascii=True)
 
         i+=1
             
